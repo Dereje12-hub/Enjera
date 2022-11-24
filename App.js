44b6d8401f4f72ec1cc463f2bc80b32941b4c60a
Dereje12-hub <58503546+Dereje12-hub@users.noreply.github.com>
2022-11-23 20:07:32 -0600

    MyStack folder is created

--- App.js
+++ App.js
@@ -1,25 +1,17 @@
 
+
+
 import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Button, Text, Animated } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>ሰላም!</Text>
-      <Text>የመጀመሪያ!</Text>
-      <StatusBar style="auto" />
-     </View>
- 
+    <NavigationContainer>
+         <View>
+           <Text>MyStack</Text>
+        </View>
+    </NavigationContainer>
   );
 }
-  
-    
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
